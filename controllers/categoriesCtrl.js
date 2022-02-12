4abdc139a134c7dc344085573c976755fb3fba97
Vladomira <juliavladomira@gmail.com>
2022-02-12 17:30:58 +0300

    fix

--- controllers/categoriesCtrl.js
+++ controllers/categoriesCtrl.js
@@ -1,12 +1,25 @@
-const operations = require('../models/category')
+const fs = require("fs/promises");
+const { NotFound } = require("http-errors");
+const path = require("path");
+const categoriesPath = path.join(__dirname, "../", "categories.json");
+
+const categoriesList = async () => {
+  try {
+    const data = await fs.readFile(categoriesPath);
+    const categories = JSON.parse(data);
+    return categories;
+  } catch (error) {
+    throw new NotFound("Not found");
+  }
+};
 
 const getAll = async (req, res, next) => {
   try {
-    const categories = await operations.categoriesList()
-    res.json(categories)
+    const categories = await categoriesList();
+    res.json(categories);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
-module.exports = { getAll }
+module.exports = { getAll };
