a64fadf8b6d6a05d347854834aa388274fa496b7
Liliia Shemet <lilka8069@gmail.com>
2022-02-14 16:54:07 +0200

    +Добавляет то что не залилось в  ПР#5

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
+const helmet = require('helmet');
+const { HttpCode } = require('./service/constants');
+const contactsRouter = require('./routes/api/contacts');
+const usersRouter = require('./routes/api/users');
+const { createAccountLimiter } = require('./service/rate-limit');
 
 // const usersRouter = require("./routes/api/users");
 const transactionsRouter = require("./routes/api/transactions");
@@ -11,22 +16,37 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.json({ limit: 10000 }));
 // app.use(express.static("public"));
 
 // app.use("/api/users", usersRouter);
 app.use("/api/transactions", transactionsRouter);
 app.use("/api/categories", categoriesRouter);
+app.use('/api/', createAccountLimiter);
+app.use('/api/users', usersRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
 });
+
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
+app.use((_req, res) => {
+  res.status(HttpCode.NOT_FOUND).json({ message: 'Not found' });
+});
+
+app.use((err, _req, res, _next) => {
+  res
+    .status(err.status || HttpCode.INTERNAL_SERVER_ERROR)
+    .json({ message: err.message });
+});
 module.exports = app;
